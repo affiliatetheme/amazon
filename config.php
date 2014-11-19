f5c8d1fea3c942f2f0e718e073f9ae6308929923
Christian Lang <cl@endcore.com>
2014-11-19 19:31:59 +0100

    AWS Country konfigurierbar über Datenbank

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 require_once ABSPATH.'/wp-load.php';
 
-define('AWS_COUNTRY', 'DE');
+define('AWS_COUNTRY', get_option('amazon_country'));
 define('AWS_API_KEY', get_option('amazon_public_key'));
 define('AWS_API_SECRET_KEY', get_option('amazon_secret_key'));
 define('AWS_ASSOCIATE_TAG', get_option('amazon_partner_id'));
