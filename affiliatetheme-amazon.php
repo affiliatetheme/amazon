682e3d5fc85bad4cefbac4aeccb47097fa6de3db
Christian Lang <cl@endcore.com>
2015-08-11 18:45:37 +0200

    Version 0.0.6 › 0.0.7

--- affiliatetheme-amazon.php
+++ affiliatetheme-amazon.php
@@ -3,7 +3,7 @@
  * Plugin Name: AffiliateTheme - Amazon Schnittstelle
  * Plugin URI: http://www.endcore.com
  * Description: Dieses Plugin erweitert das AffiliateTheme um eine Amazon Schnittstelle
- * Version: 0.0.6
+ * Version: 0.0.7
  * Author: endcore Medienagentur
  * Author URI: http://endcore.com
  * License: GPL2
