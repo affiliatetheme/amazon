031a867570145af06aa3cb9e5b202e7963418861
Christian Lang <cl@endcore.com>
2016-02-04 12:47:54 +0100

    fixed plugin updater

--- affiliatetheme-amazon.php
+++ affiliatetheme-amazon.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * Plugin Name: AffiliateTheme - Amazon Schnittstelle
- * Plugin URI: http://www.endcore.com
+ * Plugin URI: http://www.affiliatetheme.io
  * Description: Dieses Plugin erweitert das AffiliateTheme um eine Amazon Schnittstelle
- * Version: 1.1.0
+ * Version: 1.0.0
  * Author: endcore Medienagentur
  * Author URI: http://endcore.com
  * License: GPL2
@@ -17,6 +17,12 @@ if(!class_exists('AffiliateTheme_Amazon')) {
 		{
 			require_once(sprintf("%s/class.dashboard.init.php", dirname(__FILE__)));
 			$affiliatetheme_amazon_dashboard = new AffiliateTheme_Amazon_Dashboard_Init();
+
+            require 'plugin-update-checker/plugin-update-checker.php';
+            $myUpdateChecker = PucFactory::buildUpdateChecker(
+                'http://update.affiliatetheme.io/affiliatetheme-amazon.json',
+                __FILE__
+            );
 
             register_activation_hook( __FILE__, array(&$this, 'activate'));
             register_deactivation_hook( __FILE__, array(&$this, 'deactivate'));
@@ -83,32 +89,4 @@ if(!class_exists('AffiliateTheme_Amazon')) {
 if(class_exists('AffiliateTheme_Amazon'))
 {
 	$affiliatetheme_amazon = new AffiliateTheme_Amazon();
-}
-
-/**
- *  Plugin Updater
- */
-require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
-$plugin_data = get_plugin_data(__FILE__);
-$plugin_version = $plugin_data['Version'];
-
-define( 'AT_AMAZON_STORE_URL', 'http://affiliatetheme.io' );
-define( 'AT_AMAZON_ITEM_NAME', 'Amazon Schnittstelle' );
-define( 'AT_AMAZON_ITEM_ID', 19554 );
-define( 'AT_AMAZON_VERSION', $plugin_version);
-
-if( !class_exists( 'AT_Amazon_Plugin_Updater' ) ) {
-    include( dirname( __FILE__ ) . '/updater/AT_Amazon_Plugin_Updater.php' );
-}
-
-add_action( 'admin_init', 'at_amazon_plugin_updater', 0 );
-function at_amazon_plugin_updater() {
-    $license_key = '5a282e7e5109995cbae9d582936f6d7b';
-    $updater = new AT_Amazon_Plugin_Updater( AT_AMAZON_STORE_URL, __FILE__, array(
-            'version' 	=> AT_AMAZON_VERSION,
-            'license' 	=> $license_key,
-            'item_name' => AT_AMAZON_ITEM_NAME,
-            'author' 	=> 'endcore Medienagentur'
-        )
-    );
 }
