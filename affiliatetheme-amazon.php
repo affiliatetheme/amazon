ca639ccadf54863f2cf9a5e89603850a24dda2aa
Christian Lang <cl@endcore.com>
2017-02-08 08:58:39 +0100

    removed plugin form wp.org update check

--- affiliatetheme-amazon.php
+++ affiliatetheme-amazon.php
@@ -23,6 +23,8 @@ if(!class_exists('AffiliateTheme_Amazon')) {
 
             register_activation_hook( __FILE__, array(&$this, 'activate'));
             register_deactivation_hook( __FILE__, array(&$this, 'deactivate'));
+
+            add_filter('http_request_args', array(&$this, 'remove_from_wp_update'), 5, 2);
 		} 
 
 		/**
@@ -81,7 +83,38 @@ if(!class_exists('AffiliateTheme_Amazon')) {
 		public static function deactivate()
 		{
             wp_clear_scheduled_hook('affiliatetheme_amazon_api_update', array('hash' => AWS_CRON_HASH));
-		} 
+		}
+
+        /**
+         * Remove Plugin from wp.org update check
+         */
+        public function remove_from_wp_update( $r, $url )
+        {
+            // If it's not a plugins update request, bail.
+            if ( FALSE === strpos( $url, '//api.wordpress.org/plugins/update-check' ) ) {
+                return $r;
+            }
+
+            if ( empty($r['body']['plugins']) ){
+                return $r;
+            }
+
+            // Decode the JSON response
+            $plugins = json_decode( $r['body']['plugins'], true );
+
+            // Remove the plugin from the check
+            $active_index = array_search( plugin_basename( __FILE__ ), $plugins['active'] );
+            unset( $plugins['active'][ $active_index ] );
+            unset( $plugins['plugins'][ plugin_basename( __FILE__ ) ] );
+
+            // Encode the updated JSON response
+            $r['body']['plugins'] = wp_json_encode( $plugins );
+
+            var_dump($plugins);
+            exit;
+
+            return $r;
+        }
 	} 
 } 
 
