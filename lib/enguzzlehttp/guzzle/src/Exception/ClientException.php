6b380b91032e4f535b943dd4a5bb5ba1d2721001
endcoreCL <cl@endcore.com>
2019-11-19 05:59:34 +0100

    guzzle

---
+++ lib/enguzzlehttp/guzzle/src/Exception/ClientException.php
@@ -0,0 +1,7 @@
+<?php
+namespace EnGuzzleHttp\Exception;
+
+/**
+ * Exception when a client error is encountered (4xx codes)
+ */
+class ClientException extends BadResponseException {}
---
+++ lib/enguzzlehttp/guzzle/src/Exception/TransferException.php
@@ -0,0 +1,4 @@
+<?php
+namespace EnGuzzleHttp\Exception;
+
+class TransferException extends \RuntimeException implements GuzzleException {}
